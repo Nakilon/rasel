89f8e4b1275e4f2e834736eb2a4213b276f7473d
Victor Maslov <nakilon@gmail.com>
2021-10-02 13:47:00 +0300

    I don't remember how it all works though

--- constants_find.rb
+++ constants_find.rb
@@ -1,5 +1,3 @@
-# TODO: update the list for specification v2
-
 require_relative "lib/rasel"
 require "set"
 require "pcbr"
@@ -19,38 +17,36 @@ loop do
     g.map{ |n, code| [n, code, [code.size, code.delete("^0-9A-Z").chars.max || "0"]] if code.size == shortest }.
       compact.sort_by(&:last)
   end )
-  sorted = pcbr.table.reject do |_|
+  sorted = pcbr.table.reject do |_,|
     %w{
       0 1 Z 0: 01 10 1: 01- 0Z- 01\\ 1:\\ 1Z- 1Z/ Z1\\ 01-0 01-1 01-: 01\\-
       01-01- 01-1\\ 01\\-0 01\\-1 01\\-: 101-- 01-Z- 01-Z/ 0Z-1- 0Z-1\\ 0Z-Z- 1Z-1\\ 1Z-Z- 1Z-Z/ 01Z/- 10Z-/ 1:Z-/ 1Z/Z/
       1Z/1- 1Z/1\\ 1Z/Z-
-    }.include? _.first
+    }.include? _
   end.sort_by{ |_| [_.last, _.first] }
-  pp sorted[0..9]
+  pp sorted[0,10]
   pp sorted[-10..-1]
-# %w{ 0 1 Z # : $ j ? - / % \\ }.each do |c|
-  %w{ 0 1 Z   : $   ? - / % \\ }.each do |c|
+# %w{ 0 1 Z # : j ? - % / \\ }.each do |c|
+  %w{ 0 1 Z   :     - % / \\ }.each do |c|
     code = base + c
     next unless %w{ 0 1 Z }.include? code[0]
     # next unless %w{ 0 1 Z # }.include? code[0]
 
     # next if %w{ 0j ## }.include? code[-2,2]
-    next if code[/[-\/%:01Z]\$\z/]
     next if code[/0:*[-?\\\/%]\z/]
-    next if code[/[01Z](:|[01Z]-)*\?\z/]
     next if code[/[01Z](:|[01Z]-)*[1Z]%\z/]
     next if code[/[01Z](:|[01Z]-)*[01Z]1\\\z/]
     next if %w{ :% :- 1/ 00 11 ZZ }.include? code[-2,2]
     next if %w{ 0Z/ 1:/ Z:/ :1\\ Z1- }.include? code[-3,3]
-    next if %w{ 01-\\ 0Z-\\ 1Z-\\ 01\\/ 1:\\$ 01\\$ Z1\\$ 01\\% 1::\\ 01:\\ 1::/ Z::/ Z:Z/ 1:1- Z:Z- 1\\1\\ 1Z/? 0:Z/ 1-1- Z:1- Z1:\\ }.include? code[-4,4]
-    next if %w{ 0:1-% 101-% 01Z-% 01Z/% 0:Z-% 1:Z-% Z1Z-% 10Z-% 1:Z/% 1Z/1% 1Z/Z% 01-:/ 01-:\\ 0:1-/ 101-/ 0:1-- 0:1-\\ 0Z-:/ 0Z-:\\ 0Z-Z/ 1Z-:/ 1Z-:\\ 01Z-- 01Z-/ 01Z// 0:Z-/ 0:Z-- 10Z-- 1:Z-- 0:Z-\\ 1:Z-\\ 1:Z/- 1:Z// 1:Z/? 1Z/:/ 1Z/:? Z1Z/- Z01-% 1:\\1\\ Z1Z/% 1Z-1- Z0Z-% Z0Z-/ Z01-/ }.include? code[-5,5]
-    next if %w{ Z1Z//? }.include? code[-6,6]
-    next if %w{ Z1Z//Z/ Z1Z//:? Z0Z--:? }.include? code[-7,7]
+    next if %w{ 01-\\ 0Z-\\ 1Z-\\ 01\\/ 01\\% 1::\\ 01:\\ 1::/ Z::/ Z:Z/ 1:1- Z:Z- 1\\1\\ 0:Z/ 1-1- Z:1- Z1:\\ }.include? code[-4,4]
+    next if %w{ 0:1-% 101-% 01Z-% 01Z/% 0:Z-% 1:Z-% Z1Z-% 10Z-% 1:Z/% 1Z/1% 1Z/Z% 01-:/ 01-:\\ 0:1-/ 101-/ 0:1-- 0:1-\\ 0Z-:/ 0Z-:\\ 0Z-Z/ 1Z-:/ 1Z-:\\ 01Z-- 01Z-/ 01Z// 0:Z-/ 0:Z-- 10Z-- 1:Z-- 0:Z-\\ 1:Z-\\ 1:Z/- 1:Z// 1Z/:/ Z1Z/- Z01-% 1:\\1\\ Z1Z/% 1Z-1- Z0Z-% Z0Z-/ Z01-/ }.include? code[-5,5]
+    next if %w{ }.include? code[-6,6]
+    next if %w{ Z1Z//Z/ }.include? code[-7,7]
 
     next if code.start_with? "1\\"
     # next if %w{ 1\\ 1- 1% Z% Z- Z/ }.include? code[0,2]
-    # next if %w{ 01-% 0Z-% 01-/ 0Z-/ 01-- 0Z-- 1:\\? 1:\\- Z1\\- 1:\\/ Z1\\/ 1:\\$ 1:\\% Z1\\% 01\\\\ 1:\\\\ Z1\\\\ 01\\? 11\\? Z1\\? 01\\- 1Z-% 1Z-- 1Z-/ 1Z/% 1Z/- 1Z// }.include? code[0,4]
-    # next if %w{ 1:\\:/ 01\\1% 01\\1- 01\\Z% 01\\Z- 01\\Z/ 01\\:/ 1:\\1% 1:\\1- 01\\:? 01\\:\\ 1:\\:? 1:\\:\\ Z1\\Z- Z1\\Z% 1:\\Z% 1:\\Z- 1:\\Z/ 1Z-1- Z1\\Z/ Z1\\:? Z1\\:\\ Z1\\1% Z1\\1- Z1\\:/ }.include? code[0,5]
+    # next if %w{ 01-% 0Z-% 01-/ 0Z-/ 01-- 0Z-- 1:\\- Z1\\- 1:\\/ Z1\\/ 1:\\% Z1\\% 01\\\\ 1:\\\\ Z1\\\\ 01\\- 1Z-% 1Z-- 1Z-/ 1Z/% 1Z/- 1Z// }.include? code[0,4]
+    # next if %w{ 1:\\:/ 01\\1% 01\\1- 01\\Z% 01\\Z- 01\\Z/ 01\\:/ 1:\\1% 1:\\1- 01\\:\\ 1:\\:\\ Z1\\Z- Z1\\Z% 1:\\Z% 1:\\Z- 1:\\Z/ 1Z-1- Z1\\Z/ Z1\\:\\ Z1\\1% Z1\\1- Z1\\:/ }.include? code[0,5]
     # next if %w{ }.include? code[0,6]
 
     begin
